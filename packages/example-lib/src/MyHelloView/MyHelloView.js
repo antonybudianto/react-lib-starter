993b1680e8d71a0f9eec428f98409c117a9b4c39
Antony Budianto <antonybudianto@gmail.com>
2018-06-18 09:55:28 +0700

    add local global binding css

--- packages/example-lib/src/MyHelloView/MyHelloView.js
+++ packages/example-lib/src/MyHelloView/MyHelloView.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import css from './MyHelloView.css';
 
+const cx = classNames.bind(css);
+
 export const MyHelloView = props => (
-  <div className={css['my-hello-view']}>
+  <div className={cx('hello-local', 'hello-global')}>
     <h1>HelloViewLib</h1>
     <div>{props.children}</div>
   </div>
