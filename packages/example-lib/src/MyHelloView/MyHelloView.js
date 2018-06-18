a7c5f9f248a2f522bf8a78b5a58fc6c71e7252b2
Antony Budianto <antonybudianto@gmail.com>
2018-06-18 11:43:34 +0700

    remove commented code

--- packages/example-lib/src/MyHelloView/MyHelloView.js
+++ packages/example-lib/src/MyHelloView/MyHelloView.js
@@ -2,14 +2,12 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import css from './MyHelloView.css';
 import reactImg from './react.png';
-// import bigImg from './big.jpg';
 
 const cx = classNames.bind(css);
 
 export const MyHelloView = props => (
   <div className={cx('hello-local', 'hello-global')}>
     <img className={cx('hello-img')} src={reactImg} alt="React" />
-    {/* <img className={cx('hello-img')} src={bigImg} alt="React" /> */}
     <h1>HelloViewLib</h1>
     <div>{props.children}</div>
   </div>
